a23a03c4d2335fa6e7ef8743566ecec8c52f8704
Paulo Andrade <eupauloandrade@gmail.com>
2021-01-23 15:38:52 -0300

    circle.js modified. a full colored circle done

--- shapes/circle.js
+++ shapes/circle.js
@@ -6,7 +6,7 @@
 
 	ctx1.strokeStyle="lightgreen"; //circler outline color
 	ctx1.beginPath();
-	ctx1.arc(140,140,100,0*Math.PI,Math.PI*1.5,true); //values x,y,r,sAngle,eAngle,counterclockwise
+	ctx1.arc(140,140,100,100,Math.PI*2,true); //values x,y,r,sAngle,eAngle,counterclockwise
 	/*
 	 x = horizontal distance from left to the center of the circle 
 	 y = vertical distance from top to the center of the circle
